61e69938da2aba4fb6ce5a48b48462ee4372937a
Glenhawk-0 <LennonEhare@Gmail.com>
2024-11-20 08:54:41 -0500

    aMoernWindowsForm is complete

--- aModernWindowsFormsApp/Names/Names/Form1.Designer.cs
+++ aModernWindowsFormsApp/Names/Names/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnAdd = new Button();
+            lblNames = new Label();
+            lstNames = new ListBox();
+            txtName = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(138, 92);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(100, 30);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Name";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // lblNames
+            // 
+            lblNames.AutoSize = true;
+            lblNames.Location = new Point(12, 27);
+            lblNames.Name = "lblNames";
+            lblNames.Size = new Size(67, 25);
+            lblNames.TabIndex = 1;
+            lblNames.Text = "Names";
+            lblNames.Click += label1_Click;
+            // 
+            // lstNames
+            // 
+            lstNames.FormattingEnabled = true;
+            lstNames.ItemHeight = 25;
+            lstNames.Location = new Point(12, 55);
+            lstNames.Name = "lstNames";
+            lstNames.Size = new Size(120, 79);
+            lstNames.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(138, 55);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 31);
+            txtName.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(388, 221);
+            Controls.Add(txtName);
+            Controls.Add(lstNames);
+            Controls.Add(lblNames);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "Names";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnAdd;
+        private Label lblNames;
+        private ListBox lstNames;
+        private TextBox txtName;
     }
 }
--- aModernWindowsFormsApp/Names/Names/Form1.cs
+++ aModernWindowsFormsApp/Names/Names/Form1.cs
@@ -6,5 +6,16 @@ namespace Names
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
     }
 }
